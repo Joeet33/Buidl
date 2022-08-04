acd592e113d835464a6aa482d14ca3782cc0b017
Joe Ellul-Turner <joeet@pop-os.localdomain>
2022-08-04 15:05:51 +0100

    contribution activity

--- src/pages/marketplaceEmployer/index.tsx
+++ src/pages/marketplaceEmployer/index.tsx
@@ -1,11 +1,20 @@
+import GitHubCalendar from "react-github-calendar";
+import { useMoralis } from "react-moralis";
 import { StyledContainer } from "../../components/main/mainWrapper";
 import { Nav } from "../../components/nav";
 
 export const MarketPlaceEmployer = () => {
+  const { Moralis, isInitialized } = useMoralis();
+  const user = isInitialized ? Moralis.User.current() : undefined;
   return (
     <>
       <Nav />
-      <StyledContainer>employer</StyledContainer>
+      <StyledContainer>
+        employer
+        {isInitialized && (
+          <GitHubCalendar username={user && user?.attributes?.github?.login} />
+        )}
+      </StyledContainer>
     </>
   );
 };
