82dfac8196e01af421b799d9bc0dc227f1059a47
Joe Ellul-Turner <joeet@pop-os.localdomain>
2022-08-09 20:30:22 +0100

    form added

--- src/components/gitHubLogin/index.tsx
+++ src/components/gitHubLogin/index.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+import { GitHubForm } from "../settingGitHubForm";
 import { LoginContainer } from "./index.styles";
 
 export const GitHubLogin = () => {
+  const [showForm, setShowForm] = useState(false);
+
+  const handleChange = () => {
+    setShowForm(!showForm);
+  };
+
   return (
     <LoginContainer>
       <div>Add your Github to show off!</div>
-      <button>Github</button>
+      <button onClick={handleChange}>Github</button>
+      {showForm && <GitHubForm />}
     </LoginContainer>
   );
 };
---
+++ src/components/settingGitHubForm/index.tsx
@@ -0,0 +1,10 @@
+import { GitHubSaveUser } from "../settingsGitHubSaveUser";
+import { Wrapper } from "./index.styles";
+
+export const GitHubForm = () => {
+  return (
+    <Wrapper>
+      <GitHubSaveUser />
+    </Wrapper>
+  );
+};
