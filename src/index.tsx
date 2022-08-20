ccd8362f43244656d9e19785db07d7872c6ba34b
Joe Ellul-Turner <joeet@pop-os.localdomain>
2022-08-20 06:58:46 +0100

    get all users data

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,13 @@ import { BrowserRouter } from "react-router-dom";
 
 const APP_ID: string = process.env.REACT_APP_APPLICATION_ID!;
 const SERVER_URL: string = process.env.REACT_APP_SERVER_URL!;
+const MASTER_KEY: string = process.env.REACT_APP_MASTER_KEY!;
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+  <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL} >
     <BrowserRouter>
       <React.StrictMode>
         <App />
--- src/pages/applicationUser/index.tsx
+++ src/pages/applicationUser/index.tsx
@@ -1,14 +1,38 @@
+import { useEffect, useState } from "react";
 import { useMoralis } from "react-moralis";
 import { BodyContainer } from "../../components/bodyContainer/bodyContainer";
 import { Nav } from "../../components/nav";
 
 export const ApplicationUser = () => {
-  const { logout } = useMoralis();
+  const { isInitialized, Moralis } = useMoralis();
+  const user = isInitialized ? Moralis.User.current() : undefined;
+
+  const [applicationArr, setApplicationArr] = useState<any>();
+
+  useEffect(() => {
+    const asyncFunc = async () => {
+      const results = await Moralis.Cloud.run("_User");
+      console.log(results);
+      setApplicationArr(results);
+    };
+    asyncFunc();
+  }, []);
+
   return (
     <>
       <Nav />
       <BodyContainer>
-        <div>user</div>
+        <ul>
+          {applicationArr?.map((e: any, i: any) => {
+            return (
+              <>
+                {e.attributes.ethAddress === undefined ? null : (
+                  <li>{e.attributes.ethAddress}</li>
+                )}{" "}
+              </>
+            );
+          })}
+        </ul>
       </BodyContainer>
     </>
   );
